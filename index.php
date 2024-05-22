de6bcb461c49c2b547d6bc2dd78b676a592bed6e
juan andres <juanandresrivas32@gmail.com>
2024-05-21 20:32:56 -0500

    primer commit

---
+++ index.php
@@ -0,0 +1,195 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/style.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <title>.::PORTAFOLIO::.</title>
+</head>
+<body>
+    <header>
+        <a href="#" class="logo">J<span>RIVAS</span></a>
+        <ul class="navbar">
+            <li>
+                <a href="#home">Inicio</a>
+            </li>
+            <li>
+                  <a href="#about">Acerca de</a>
+            </li>
+            <li>
+                <a href="#to_buy">Estudios</a>
+            </li>
+            <li>
+                <a href="#to_sell">¿Por Qué Elegirme?</a>
+            </li>
+            <li>
+                <a href="#news">Contacto</a>
+            </li>
+        </ul>
+        <div class="redes_sociales">
+            <a href="#">Síguenos en:</a>
+            <a href="#"><i class='bx bxl-facebook'></i></a>
+            <a href="#"><i class='bx bxl-instagram' ></i></a>
+            <a href="#"><i class='bx bxl-youtube' ></i></a>
+            <div class='bx bx-menu' id="menu-icon"></div>
+        </div>
+    </header>
+    <section class="home" id="home">
+        <div class="home_text">
+            <h2>Bienvenidos</h2>
+            <img src="./images/fot_perfil-removebg-preview.png">
+            <p>Soy Juan Andres, un apasionado a diseñador web y desarrollador con experiencia en crear soluciones digitales innovadoras y efectivas. En este portafolio, encontrarás una muestra de mis trabajos más destacados, que reflejan mi habilidad para combinar estética y funcionalidad en cada proyecto</p>
+            <a href="#" class="btn">Ver más</a>
+        </div>
+    </section>
+    <section class="feature">
+        <div class="content">
+            <div class="row">
+                <div class="images">
+                    <img src="./images/diseñoweb.webp" alt="">
+                </div>
+                <h4>Diseño Web</h4>
+            </div>
+            <div class="row">
+                <div class="images">
+                    <img src="./images/DesarrolloFront-End.png" alt="">
+                </div>
+                <h4>Desarrollo Front-End</h4>
+            </div>
+            <div class="row">
+                <div class="images">
+                    <img src="./images/backend.jpg" alt="">
+                </div>
+                <h4>Desarrollo Back-End</h4>
+            </div>
+            <div class="row">
+                <div class="images">
+                    <img src="./images/DiseñoGráfico.jpg" alt="">
+                </div>
+                <h4>Diseño Gráfico</h4>
+            </div>
+            <div class="row">
+                <div class="images">
+                    <img src="./images/desarrollofuckstar.jpeg" alt="">
+                </div>
+                <h4>Desarrollo Full Stack</h4>
+            </div>
+        </div>
+    </section>
+    <section class="about" id="about">
+        <div class="about_img">
+            <img src="./images/logo_juan-removebg-preview.png" alt="">
+        </div>
+        <div class="about_text">
+            <h5>Acerca de</h5>
+            <h2>Juan Andres Rivas</h2>
+            <p>Un desarrollador web dedicado a transformar ideas en soluciones digitales eficaces y atractivas. Ofrezco una amplia gama de servicios de programación web personalizados para satisfacer las necesidades de tu negocio y ayudarte a destacarte en el mundo digital.</p>
+            <a href="#" class="btn">Leer más</a>
+        </div>
+    </section>
+    <section class="to_buy" id="to_buy">
+        <div class="title">
+            <h2>Estudios</h2>
+        </div>
+        <div class="content-image">
+            <div class="imagen">
+                <img src="./images/certificado1.jpeg" alt="">
+                <div class="overlay">
+                    <h2>IMB<p>2023</p></h2>
+                </div>
+            </div>
+            <div class="imagen">
+                    <img src="./images/certificado6.jpeg" alt="">
+                    <div class="overlay">
+                        <h2>Google cloud <p>2023</p></h2>
+                    </div>
+            </div>
+            <div class="imagen">
+                    <img src="./images/certificado2.jpeg" alt="">
+                    <div class="overlay">
+                        <h2>Manejo de residuos <P>2022</P></h2>
+                    </div>
+            </div>
+            <div class="imagen">
+                    <img src="./images/certificado3.jpeg" alt="">
+                    <div class="overlay">
+                        <h2>Tecnico en sistemas <p>2021</p></h2>
+                    </div>
+            </div>
+            <div class="imagen">
+                    <img src="./images/certificado4.jpeg" alt="">
+                    <div class="overlay">
+                        <h2>Emprendimiento digital <p>2021</p></h2>
+                    </div>
+            </div>
+            <div class="imagen">
+                    <img src="./images/certificas5.jpeg" alt="">
+                    <div class="overlay">
+                        <h2>Manejo de excel <p>2021</p></h2>
+                    </div>
+            </div>
+        </div>
+    </section>
+    <section class="to_sell" id="to_sell">
+        <div class="to_sell_text">
+            <h5>Acerca de</h5>
+            <h2>¿Por Qué Elegirme?</h2>
+            <p><span>Experiencia</span> Más de 1 años de experiencia en la industria del desarrollo web y diseño gráfico.</p>
+            <p><span>Mi enfoque</span> se basa en la innovación y en encontrar soluciones únicas para cada desafío.</p>
+            <p><span>Compromiso</span> Trabajo de cerca con mis clientes para asegurar que cada proyecto cumpla y supere sus expectativas.</p>
+            <p><span>Capacidad</span> para adaptarme a diferentes estilos y requisitos, ofreciendo soluciones personalizadas para cada necesidad.</p>
+            <a href="#" class="btn">Leer más</a>
+        </div>
+        <div class="to_sell_img">
+            <img src="./images/logo_juan-removebg-preview.png" alt="">
+        </div>
+    </section>
+    <section class="news" id="news">
+        <div class="news_content">
+            <div class="news_text">
+                <h2>Ponte en Contacto</h2>
+                <p>¿Tienes un proyecto en mente? Estoy aquí para ayudarte a hacerlo realidad. <span>3223340060</span> para discutir cómo podemos trabajar juntos y crear algo increíble.</p>
+            </div>
+            <form action="#">
+                <input type="email" placeholder="Ingresa tu correo electrónico">
+                <input type="submit" value="Enviar" class="btn">
+            </form>
+        </div>
+    </section>
+    <footer>
+        <div class="footer_box">
+            <h3>Servicios</h3>
+            <a href="#">Desarrollo de Sitios Web Personalizados</a>
+            <a href="#">Asesoramiento</a>
+            <a href="#">Mantenimiento y Soporte</a>
+            <a href="#">Desarrollo de Aplicaciones Web</a>
+        </div>
+        <div class="footer_box">
+            <h3>Nosotros</h3>
+            <a href="#">Nuestra historia</a>
+            <a href="#">Política</a>
+            <a href="#">Colaboradores</a>
+            <a href="#">Términos</a>
+        </div>
+        <div class="footer_box">
+            <h3>Ayudas</h3>
+            <a href="#">FAQ's</a>
+            <a href="#">Contáctenos</a>
+        </div>
+        <div class="footer_box">
+            <h3>Redes Sociales</h3>
+                <div class="social">
+                    <a href="#"><i class='bx bxl-facebook'></i></a>
+                    <a href="#"><i class='bx bxl-instagram' ></i></a>
+                    <a href="#"><i class='bx bxl-youtube' ></i></a>
+                </div>
+        </div>
+    </footer>
+    <div class="copyright">
+        <p>Copyright 2024 por Juan Andres Rivas  VI Semestre - <span>UNIMINUTO</span> - Ingeniería de Sistemas </p>
+    </div>
+    <script src="scritp/script.js"></script>
+</body>
+</html>
